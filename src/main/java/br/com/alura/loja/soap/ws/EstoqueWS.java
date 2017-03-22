6f7f375c8e4d86f0db826cfeca60ada7d49c7f24
Ana Siqueira <paulabia80@gmail.com>
2017-03-22 19:18:58 -0300

    SOAP

--- src/main/java/br/com/alura/loja/soap/ws/EstoqueWS.java
+++ src/main/java/br/com/alura/loja/soap/ws/EstoqueWS.java
@@ -1,43 +1,45 @@
 package br.com.alura.loja.soap.ws;
-
-import br.com.alura.loja.soap.Item.Item;
-import br.com.alura.loja.soap.Item.ItemDao;
+import br.com.alura.loja.soap.exception.AutorizacaoException;
+import br.com.alura.loja.soap.modelo.Item.*;
+import br.com.alura.loja.soap.modelo.usuario.TokenDao;
+import br.com.alura.loja.soap.modelo.usuario.TokenUsuario;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
-import javax.xml.ws.RequestWrapper;
-import javax.xml.ws.ResponseWrapper;
 import java.util.List;
 
-/**
- * Created by apq on 07/03/17.
- */
+
 @WebService
 public class EstoqueWS {
 
-    private ItemDao dao = new ItemDao();
+	private ItemDao dao = new ItemDao();
 
+	@WebMethod(operationName = "todosOsItens")
+	@WebResult(name = "itens")
+	public ListaItens getItens(@WebParam(name = "filtros") Filtros filtros) {
+		System.out.println("Chamando todosItens()");
+		List<Filtro> lista = filtros.getLista();
+		List<Item> itensResultado = dao.todosItens(lista);
+		return new ListaItens(itensResultado);
+	}
 
-//    @WebMethod(operationName="todosOsItens")
-//    @WebResult(name="itens")
-//    @ResponseWrapper(localName = "itens")
-//    public ListaItens getItens(@WebParam(name="filtros") Filtros filtros) { //cuidado, plural
-//        System.out.println("Chamando getItens()");
-//        List<Filtro> lista = filtros.getLista();
-//        List<Item> itensResultado = dao.todosItens(lista);
-//        return new ListaItens(itensResultado);
-//    }
+	@WebMethod(operationName = "CadastrarItem")
+	public Item cadastrarItem(
+			@WebParam(name = "tokenUsuario", header = true) TokenUsuario token,
+			@WebParam(name = "item") Item item) throws AutorizacaoException {
 
-    @WebMethod(operationName="todosOsItens")
-    @ResponseWrapper(localName="itens")
-    @WebResult(name="item")
-    @RequestWrapper(localName="listaItens")
-    public List<Item> getItens() {
+		if (!new TokenDao().ehValido(token)) {
+			throw new AutorizacaoException("Autorizacao falhou");
+		}
 
-        System.out.println("Chamando getItens()");
-        return dao.todosItens();
+		System.out.println("Cadastrando " + item + ", " + token);
 
-    }
+		new ItemValidador(item).validate();
+		
+		this.dao.cadastrar(item);
 
+		return item;
+	}
 }
